4bf1a79db2869a1f545eb86590ee73b24f4b1b60
gluay88 <p.wantagaew@gmail.comm>
2022-10-16 20:24:08 -0600

    delete confirm modal

--- src/Pages/map/Site.js
+++ src/Pages/map/Site.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import EditIcon from "../../assets/edit-icon.svg";
 import DeleteIcon from "../../assets/delete-icon.svg";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const Site = ({ siteName, id, deleteSite }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div className="create-site-container">
-      <div className="site-name-content">
-        <a href="#">{siteName} &#x25B6;</a>
+    <>
+      <div className="create-site-container">
+        <div className="site-name-content">
+          <a href="#">{siteName} &#x25B6;</a>
+        </div>
+        <div>
+          <button>
+            <img src={EditIcon} alt="edit-icon" />
+          </button>
+          {/* <button onClick={() => deleteSite(id)}>
+            <img src={DeleteIcon} alt="delete-icon" width="14px" />
+          </button> */}
+
+          <Button onClick={handleShow} className="delete-site-btn">
+            <img src={DeleteIcon} alt="delete-icon" width="14px" />
+          </Button>
+
+          <Modal show={show} onHide={handleClose} className="delete-site-popup">
+            <Modal.Header closeButton>
+              <Modal.Title>Are you ABSOLUTELY sure?</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>Please type in the name of the site to confirm</p>
+              <input type="text" placeholder="Type site name.." />
+              <br />
+              <p>Everything associated to the site will be deleted</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => deleteSite(id)}>DELETE SITE</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
-      <div>
-        <button>
-          <img src={EditIcon} alt="edit-icon" />
-        </button>
-        <button onClick={() => deleteSite(id)}>
-          <img src={DeleteIcon} alt="delete-icon" width="14px" />
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/map/User.js
+++ src/Pages/map/User.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import EditIcon from "../../assets/edit-icon.svg";
 import DeleteIcon from "../../assets/delete-icon.svg";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const User = ({ userName, id, deleteUser }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="create-user-container">
       <div className="user-name-btn">
@@ -13,9 +18,27 @@ const User = ({ userName, id, deleteUser }) => {
         <button>
           <img src={EditIcon} alt="edit-icon" className="edit-svg" />
         </button>
-        <button onClick={() => deleteUser(id)}>
+        {/* <button onClick={() => deleteUser(id)}>
           <img src={DeleteIcon} alt="delete-icon" />
-        </button>
+        </button> */}
+        <Button onClick={handleShow} className="delete-site-btn">
+          <img src={DeleteIcon} alt="delete-icon" width="14px" />
+        </Button>
+
+        <Modal show={show} onHide={handleClose} className="delete-site-popup">
+          <Modal.Header closeButton>
+            <Modal.Title>Are you ABSOLUTELY sure?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Please type in the name of the user to confirm</p>
+            <input type="text" placeholder="Type user name.." />
+            <br />
+            <br />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => deleteUser(id)}>DELETE USER</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   );
